ce45b12bbbe31d9653605d9cd36713a0a905a7be
tedtalktimmy <frtntn007@gmail.com>
2022-11-15 12:47:05 -0500

    changed the resume/linkedin link to open in new tab

--- pages/index.js
+++ pages/index.js
@@ -45,6 +45,7 @@ export default function Home() {
               </li>
               <li>
                 <a
+                  target="_blank"
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
                   href="https://www.linkedin.com/in/fortunatonatanael/"
                 >
