210648d7108e60c18fa9763d4b9f5bfa548a8cb2
tedtalktimmy <frtntn007@gmail.com>
2022-11-15 12:33:26 -0500

    added more projects to my list

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6f488f5aecd0868a"; }
+/******/ 		__webpack_require__.h = function() { return "98c2b7c5419bc619"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ .next/static/webpack/pages/index.e1cdef67684d96b1.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/index",{
+
+/***/ "./public/tmPrflgen.png":
+/*!******************************!*\
+  !*** ./public/tmPrflgen.png ***!
+  \******************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\"src\":\"/_next/static/media/tmPrflgen.1e3162d2.png\",\"height\":1820,\"width\":2992,\"blurDataURL\":\"/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FtmPrflgen.1e3162d2.png&w=8&q=70\",\"blurWidth\":8,\"blurHeight\":5});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wdWJsaWMvdG1QcmZsZ2VuLnBuZy5qcyIsIm1hcHBpbmdzIjoiO0FBQUEsK0RBQWUsQ0FBQyx3TUFBd00iLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vcHVibGljL3RtUHJmbGdlbi5wbmc/ZWIwMSJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCB7XCJzcmNcIjpcIi9fbmV4dC9zdGF0aWMvbWVkaWEvdG1QcmZsZ2VuLjFlMzE2MmQyLnBuZ1wiLFwiaGVpZ2h0XCI6MTgyMCxcIndpZHRoXCI6Mjk5MixcImJsdXJEYXRhVVJMXCI6XCIvX25leHQvaW1hZ2U/dXJsPSUyRl9uZXh0JTJGc3RhdGljJTJGbWVkaWElMkZ0bVByZmxnZW4uMWUzMTYyZDIucG5nJnc9OCZxPTcwXCIsXCJibHVyV2lkdGhcIjo4LFwiYmx1ckhlaWdodFwiOjV9OyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./public/tmPrflgen.png\n"));
+
+/***/ })
+
+});
